2bc1df6e7edc59251f2bdfaaeec398db0d29ec63
Arthur BRUNOT <arthur.brunot.pro@gmail.com>
2021-03-11 14:34:41 +0100

    update

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+/*
+if($_POST['isConnected'] == 1 || $_POST['isConnected'] == null) {
+	header('Location: auth.php');
+}
+*/
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta http-equiv="X-UA-Compatible" content="chrome=1, IE=edge">
+	<title>Title</title>
+
+	<style type="text/css">
+        body {
+		margin: 0;
+		padding: 0;
+	}
+	/*  
+		Disable the default callout when you touch and hold a touch target.
+		and disable user from selecting content (dragging).
+	*/
+	*:not(input,textarea) {
+		-webkit-touch-callout: none;
+		-webkit-user-select: none; 
+		-khtml-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+	</style>
+</head>
+<body>
+    
+</body>
+</html>
